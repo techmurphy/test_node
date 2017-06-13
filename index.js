7668ba0acf5cb16fa7b957dd5de36834b9ad2da8
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-13 21:48:10 +0200

    Create index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
@@ -10,4 +10,15 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-response.end("Server running at http://localhost:%d", port);
+response.end("Server running at http://localhost:%d", port);*/
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
